96a3b2dbf1ff37074a8f73e5f96b47b4b67c6db3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-19 13:32:31 +0000

    Deploy 1134506391/jie-ui to github.com/1134506391/jie-ui.git:gh-pages

---
+++ js/chunk-2d0cfa15.2bf4ae3a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cfa15"],{6511:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" home "),t("j-button",[n._v("aa")]),t("cc-icon",{attrs:{name:"cc-edit"}})],1)},a=[],o={name:"home"},u=o,i=t("2877"),l=Object(i["a"])(u,c,a,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0cfa15.2bf4ae3a.js.map
